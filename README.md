e87a66712a7ff221d7956e3f36c9c0bef7ed8a66
aneeshcheriank <aneeshcheriank@gmail.com>
2024-12-23 09:45:58 +0100

    updated additional reading

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Music-Classification-Rust
 Classify lyrics using HF model in Rust
+
+- [Rust GPU](nogibjj/rust-pytorch-gpu-template)
+- carets io (rust based lib for machine learning)
+
+## ONNEX
+- package ml models
+- use models between various frameworks
+
+- ![alt text](image.png)
+
+## Additional Reading
+
+|Title|Type|Length<br>minutes|Brief Description|
+|---|---|---|---|
+|[Code examples in Rust MLOps Template](https://github.com/nogibjj/rust-mlops-template)|interactive Tutorial|30-60|Examples of many Rust MLOps projects for students to explore.|
+|[Code examples in Rust PyTorch bindings](https://github.com/LaurentMazare/tch-rs)|Interactive Tutorial|30-60|Examples of PyTorch and Rust working together and worth students exploring.|
+|[COde examples in Rust Stable Diffusion Repo](https://github.com/LaurentMazare/diffusers-rs)|Interactive Tutorial|30-60|Examples of Stable Diffusion and Rust working together and worth students exploring.|
+|[Introduction to ONNEX](https://onnx.ai/onnx/intro/)|Documentation|5-10|The official documentation guide to ONNX, which is a portable model format that is increasingly important to learn.|
+|[Using EFS with AWS Lambda](https://aws.amazon.com/blogs/compute/using-amazon-efs-for-aws-lambda-in-your-serverless-applications/)|Reading|5-10|An official guide to using EFS for AWS Lambda, helpful to look at the key touchpoints for configuring EFS.|
+|[Rust for MLOps GitHub README project](https://docs.google.com/document/d/1Zp3K384gFMYYMx7nKCoWxTdLNv7KVaywivrGge7opnA/edit?usp=sharing)|Reading|5-10|A brief summary about the key reasons for Rust for MLOps.|
+
+## HuggingFace Chatbot Arena
+
+## Reflections
--- src/lib.rs
+++ src/lib.rs
@@ -7,15 +7,20 @@ use std::io::BufReader;
 // create zeor short classification candidates
 fn create_db() -> sqlite::Connection {
     let db = sqlite::Connection::open_in_memory().unwrap();
-    db.execute("CREATE TABLE zeroshortcandidates (id INTEGER PRIMARY KEY, label TEXT)").unwrap();
-    db.execute("INSERT INTO zeroshortcandidates (label) VALUES ('rock')").unwrap();
-    db.execute("INSERT INTO zeroshortcandidates (label) VALUES ('pop')").unwrap();
-    db.execute("INSERT INTO zeroshortcandidates (label) VALUES ('hip hop')").unwrap();
-    db.execute("INSERT INTO zeroshortcandidates (label) VALUES ('country')").unwrap();
-    db.execute("INSERT INTO zeroshortcandidates (label) VALUES ('latin')").unwrap();
+    db.execute("CREATE TABLE zeroshortcandidates (id INTEGER PRIMARY KEY, label TEXT)")
+        .unwrap();
+    db.execute("INSERT INTO zeroshortcandidates (label) VALUES ('rock')")
+        .unwrap();
+    db.execute("INSERT INTO zeroshortcandidates (label) VALUES ('pop')")
+        .unwrap();
+    db.execute("INSERT INTO zeroshortcandidates (label) VALUES ('hip hop')")
+        .unwrap();
+    db.execute("INSERT INTO zeroshortcandidates (label) VALUES ('country')")
+        .unwrap();
+    db.execute("INSERT INTO zeroshortcandidates (label) VALUES ('latin')")
+        .unwrap();
     db
 }
-
 
 pub fn get_all_zeroshotcandidates() -> Vec<String> {
     let db = create_db();
@@ -33,7 +38,6 @@ pub fn get_all_zeroshotcandidates() -> Vec<String> {
     candidates
 }
 
-
 // read lyrics form a file and return a vector of stings
 pub fn read_lyrics(file: &str) -> Vec<String> {
     let mut lyrics: Vec<String> = Vec::new();
@@ -45,7 +49,6 @@ pub fn read_lyrics(file: &str) -> Vec<String> {
     }
     lyrics
 }
-
 
 /* use hugging face to classify lyrics using zero short classification
 Accepts a vector of strings as lyrics and grabs candidates from the in memory sqlite database
